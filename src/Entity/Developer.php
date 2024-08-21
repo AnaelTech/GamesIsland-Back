c11627382841b1fd84617e4de9a40f4d1bfe553a
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-08-21 13:46:57 +0200

    Set entities as API resources

--- src/Entity/Developer.php
+++ src/Entity/Developer.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use App\Repository\DeveloperRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: DeveloperRepository::class)]
+#[ApiResource()]
 class Developer
 {
     #[ORM\Id]
--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use App\Repository\GameRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: GameRepository::class)]
+#[ApiResource()]
 class Game
 {
     #[ORM\Id]
